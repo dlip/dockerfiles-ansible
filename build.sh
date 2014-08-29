76d187d4ecab9b9ef1ac008a3586f5d012ae9449
Dane Lipscombe <dane@lipscombe.com.au>
2014-08-29 11:47:14 +0900

    Add postgresql

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+NAME=$1
 for dir in $(find ./* -maxdepth 0 -type d | sed 's/\.\///') ; do
-  echo "Building $dir."
-  docker build -t dlip/$dir $dir
+  if [ -z "$NAME" ] || [ "$NAME" == "$dir" ]; then
+    echo "Building $dir."
+    docker build -t dlip/$dir $dir
+  fi
   if [ $? -ne 0 ]; then
     echo "Build fail."
     exit 1
